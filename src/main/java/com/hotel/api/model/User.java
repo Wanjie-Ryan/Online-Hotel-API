dd0ff4ce471e2308d5d22d69ba91e316dffa946f
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-11 19:39:00 +0300

    defining the models - user model

--- src/main/java/com/hotel/api/model/User.java
+++ src/main/java/com/hotel/api/model/User.java
@@ -1,4 +1,30 @@
 package com.hotel.api.model;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity // map this java class to our user table in the db.
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table (name = "user")
 public class User {
+
+    @Id
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+    private User_Role role;
+
+
+
+
 }
