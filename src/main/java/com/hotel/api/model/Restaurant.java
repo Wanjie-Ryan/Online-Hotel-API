872f36c3da433935e024c29fa109be5645aad7d6
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-12 15:20:44 +0300

    defining the models - restaurant model

--- src/main/java/com/hotel/api/model/Restaurant.java
+++ src/main/java/com/hotel/api/model/Restaurant.java
@@ -1,10 +1,12 @@
 package com.hotel.api.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -34,8 +36,20 @@ public class Restaurant {
 
     private String openingHours;
 
-    @OneToMany(mappedBy = "restaurant")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true, mappedBy = "restaurant") // this tells spring not to create a new table for the orders but reference the restaurant in the orders table
     private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String> images;
+
+    private LocalDateTime registrationDate;
+
+    private boolean open;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
+    private List<Food> food  = new ArrayList<>();
 
     @PrePersist
 
