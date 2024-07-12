a26aa9713033ee6ecb8bb6babeb78bdef6eb68fd
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-12 18:42:40 +0300

    moving to user authentication

--- src/main/java/com/hotel/api/repository/UserRepository.java
+++ src/main/java/com/hotel/api/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.hotel.api.repository;
 
-public interface UserRepository {
+import com.hotel.api.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+// provide the entity name, and the unique identifier type for the entity
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
 }
