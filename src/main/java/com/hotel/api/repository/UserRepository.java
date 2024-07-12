63104ddfc7031bd7d007a233be3a8425a267199a
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-12 18:46:32 +0300

    moving to user authentication

--- src/main/java/com/hotel/api/repository/UserRepository.java
+++ src/main/java/com/hotel/api/repository/UserRepository.java
@@ -8,5 +8,7 @@ import java.util.UUID;
 // provide the entity name, and the unique identifier type for the entity
 public interface UserRepository extends JpaRepository<User, UUID> {
 
+    // custom method to find user by email
+    public User findByEmail(String email);
 
 }
