22813a23618052dd0c0f188d59a2a93713de13c8
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-13 12:35:27 +0300

    moving to user authentication

---
+++ src/main/java/com/hotel/api/config/JwtConstant.java
@@ -0,0 +1,12 @@
+package com.hotel.api.config;
+
+// we define the header name and the key
+public class JwtConstant {
+
+    // the JWT SECRET
+    public static final String SECRET_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZSI6IkphdmFJblVzZSIsImV4cCI6MTcyMDA3OTQyNiwiaWF0IjoxNzIwMDc5NDI2fQ.WQDomcG9AXavXxDJyPFD4avcI7arLaBFelSsbHf32Qk";
+
+    // setting the name of the JWT_Header
+    public static final String JWT_HEADER = "Authorization";
+
+}
