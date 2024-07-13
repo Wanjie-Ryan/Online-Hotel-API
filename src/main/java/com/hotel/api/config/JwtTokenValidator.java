18145d965884264190d51c1a23019f91d71adb4e
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-13 12:14:43 +0300

    moving to user authentication

--- src/main/java/com/hotel/api/config/JwtTokenValidator.java
+++ src/main/java/com/hotel/api/config/JwtTokenValidator.java
@@ -1,4 +1,19 @@
 package com.hotel.api.config;
 
-public class JwtTokenValidator {
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
+
+    }
 }
