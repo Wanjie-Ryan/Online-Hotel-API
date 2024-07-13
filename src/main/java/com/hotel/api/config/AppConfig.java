c30a8829e23b36ce6b2b90bd3c4e6810d85cc788
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-13 11:51:09 +0300

    moving to user authentication

--- src/main/java/com/hotel/api/config/AppConfig.java
+++ src/main/java/com/hotel/api/config/AppConfig.java
@@ -6,12 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 //configuring spring security in the AppConfig for user authentication.
 // it is a configuration class therefore annotate it with configuration.
@@ -41,17 +44,34 @@ public class AppConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
 
                 CorsConfiguration cfg = new CorsConfiguration();
+                // allowed routes that communicate with the BE.
                 cfg.setAllowedOrigins(Arrays.asList(
                         "http://localhost:3000",
                         "http://localhost:3001"
                 ));
 
+                // Allowed HTTP methods
+                cfg.setAllowedMethods(Collections.singletonList("*"));
+                // Allow the credentials
+                cfg.setAllowCredentials(true);
+                cfg.setAllowedHeaders(Collections.singletonList("*"));
+                // the exposed headers is necessary so that we can be able to send the jwt in the headers.
+                cfg.setExposedHeaders(Arrays.asList("Authorization"));
+                cfg.setMaxAge(3600L);
 
-                return null;
+
+                return cfg;
             }
         };
 
     }
+
+    // here we set the password encoder, bcrypt.
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
 
 
 }
