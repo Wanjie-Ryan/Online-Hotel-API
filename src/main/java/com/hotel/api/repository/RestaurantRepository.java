2faffe0004b7541910d96ba57d51200b05e10243
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-16 07:39:34 +0300

    Restaurant API -section

--- src/main/java/com/hotel/api/repository/RestaurantRepository.java
+++ src/main/java/com/hotel/api/repository/RestaurantRepository.java
@@ -2,6 +2,7 @@ package com.hotel.api.repository;
 
 import com.hotel.api.model.Restaurant;
 import com.hotel.api.model.User;
+import com.hotel.api.request.CreateRestaurantRequest;
 
 public interface RestaurantRepository {
 
--- src/main/java/com/hotel/api/request/CreateRestaurantRequest.java
+++ src/main/java/com/hotel/api/request/CreateRestaurantRequest.java
@@ -1,4 +1,22 @@
 package com.hotel.api.request;
 
+import com.hotel.api.model.Address;
+import com.hotel.api.model.ContactInformation;
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
 public class CreateRestaurantRequest {
+    private UUID id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private Address address;
+    private ContactInformation contactInformation;
+    private String openingHours;
+    private List<String> images;
+
+
 }
