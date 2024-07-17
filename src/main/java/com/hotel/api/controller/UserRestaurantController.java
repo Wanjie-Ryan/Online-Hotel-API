7ad94fa8fed421d02772690a4347d8807ad84959
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-17 16:23:12 +0300

    Restaurant API -section

--- src/main/java/com/hotel/api/controller/UserRestaurantController.java
+++ src/main/java/com/hotel/api/controller/UserRestaurantController.java
@@ -4,12 +4,14 @@ import com.hotel.api.model.Restaurant;
 import com.hotel.api.model.User;
 import com.hotel.api.service.RestaurantService;
 import com.hotel.api.service.UserService;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/user-restaurant")
@@ -31,4 +33,27 @@ public class UserRestaurantController {
         return new ResponseEntity<>(hotel, HttpStatus.OK);
 
     }
+
+    @GetMapping("/allhotels")
+
+    public ResponseEntity <List<Restaurant>> getAllHotels(@RequestHeader("Authorization") String jwt) throws Exception{
+
+        User user = userService.findUserByJwt(jwt);
+        List<Restaurant> hotels = hotelService.getAllRestaurants();
+
+        return new ResponseEntity<>(hotels, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+
+    public ResponseEntity<Restaurant> getHotelById(@PathVariable("id") UUID id, @RequestHeader("Authorization") String jwt) throws Exception{
+
+        User user = userService.findUserByJwt(jwt);
+        Restaurant hotel = hotelService.findRestaurantById(id);
+
+        return new ResponseEntity<>(hotel, HttpStatus.OK);
+
+    }
+
+
 }
