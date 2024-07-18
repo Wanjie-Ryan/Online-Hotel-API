e6c006b8966065794736f536c941f65e49821f45
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-18 19:00:44 +0300

    Food API

--- src/main/java/com/hotel/api/repository/FoodRepo.java
+++ src/main/java/com/hotel/api/repository/FoodRepo.java
@@ -2,6 +2,8 @@ package com.hotel.api.repository;
 
 import com.hotel.api.model.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -9,4 +11,6 @@ import java.util.UUID;
 public interface FoodRepo extends JpaRepository<Food, UUID> {
 
     public List<Food> findByRestaurantId(UUID id);
+    @Query("SELECT f FROM Food f WHERE f.name LIKE %:keyword% OR f.foodCategory.name LIKE %:keyword%")
+    public List<Food> searchFood(@Param("keyword") String keyword);
 }
