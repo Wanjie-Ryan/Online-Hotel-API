1891d53ae39dd1761ee48e260f883f53df34800b
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-19 11:51:17 +0300

    Food API

--- src/main/java/com/hotel/api/controller/FoodController.java
+++ src/main/java/com/hotel/api/controller/FoodController.java
@@ -1,10 +1,33 @@
 package com.hotel.api.controller;
 
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.hotel.api.model.Food;
+import com.hotel.api.model.User;
+import com.hotel.api.service.FoodService;
+import com.hotel.api.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class FoodController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private FoodService foodService;
+
+
+    @GetMapping("/search")
+
+    public ResponseEntity<List<Food>> searchFood(@RequestParam("keyword") String keyword, @RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserByJwt(jwt);
+        List<Food> foundFoods = foodService.searchFood(keyword);
+        return new ResponseEntity<>(foundFoods, HttpStatus.OK);
+    }
+
 }
