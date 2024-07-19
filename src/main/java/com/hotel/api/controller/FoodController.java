7d262575dcc4a66f5124a6bb19e62a0ebef7fa9c
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-07-19 12:03:35 +0300

    Food API

--- src/main/java/com/hotel/api/controller/FoodController.java
+++ src/main/java/com/hotel/api/controller/FoodController.java
@@ -10,9 +10,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api/food")
 public class FoodController {
 
     @Autowired
@@ -24,10 +25,23 @@ public class FoodController {
 
     @GetMapping("/search")
 
-    public ResponseEntity<List<Food>> searchFood(@RequestParam("keyword") String keyword, @RequestHeader("Authorization") String jwt) throws Exception{
+    public ResponseEntity<List<Food>> searchFood(@RequestParam String keyword, @RequestHeader("Authorization") String jwt) throws Exception{
         User user = userService.findUserByJwt(jwt);
         List<Food> foundFoods = foodService.searchFood(keyword);
         return new ResponseEntity<>(foundFoods, HttpStatus.OK);
+    }
+
+    // finding the food for the hotel
+    @GetMapping("/hotel/{hotelId")
+
+    public ResponseEntity<List<Food>> getHotelFoods(@PathVariable("hotelId") UUID hotelId, @RequestParam boolean vegetarian, @RequestParam boolean seasonal, @RequestParam(required = false) String food_category, @RequestParam boolean available ,@RequestHeader("Authorization") String jwt) throws Exception {
+
+        User user = userService.findUserByJwt(jwt);
+
+        List<Food> foods = foodService.getAllFoods(hotelId, vegetarian, seasonal, food_category, available);
+
+
+        return new ResponseEntity<>(foods, HttpStatus.OK);
     }
 
 }
